02f556b2b702eae6cf5224b9b4f4251fc2ea84d3
djx <dongjinxianaugust@gmail.com>
2018-04-09 16:24:22 +0800

    1、数据集图片尺寸探索分析

--- scripts/diagram_data.py
+++ scripts/diagram_data.py
@@ -6,6 +6,10 @@ from PIL import Image
 
 # 读取数据集各个图片的大小，并保存到文件中
 def load_data_size(data_path, name):
+    # 文件已经存在
+    if os.path.exists("data_%s.xlsx"%name):
+        return
+
     # 读取宽高
     all_data = []
     for i in range(1,12499):
@@ -23,14 +27,35 @@ def load_data_size(data_path, name):
     df.to_excel("data_%s.xlsx"%name, index=False)
 
 # 绘制散点图
-def draw(file):
-    pass
+def draw(file, name):
+    # 读取数据
+    data = pd.read_excel(file)
+    print(data.head())
+    high = list(data["high"])
+    width = list(data["width"])
+
+    # 绘图
+    fig = plt.figure(0)
+    ax1 = fig.add_subplot(111)
+    ax1.set_title(name)
+    plt.xlabel('width')
+    plt.ylabel('high')
+    ax1.scatter(width, high, marker='.', color = 'r')
+    # ax1.scatter(width, high, marker='.')
+    plt.legend('x1')
+    plt.show()
 
 # main
 def main():
     # 读取图片信息，并记录到文件，供后续处理
     load_data_size("../../temp/competitions/dogs-vs-cats-redux-kernels-edition", "dog")
     load_data_size("../../temp/competitions/dogs-vs-cats-redux-kernels-edition", "cat")
+
+    # 绘制散点图
+    # draw("data_cat.xlsx", "Figure_Cat_Raw");
+    # draw("data_cat_x.xlsx", "Figure_Cat_Modif");
+    # draw("data_dog.xlsx", "Figure_Dog_Raw");
+    # draw("data_dog_x.xlsx", "Figure_Dog_Modif");
 
 # 运行
 if __name__ == '__main__':
